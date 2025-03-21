8c67d899c05b8696029095244c14810fb5aea029
группы стационара <st@itstep.lan>
2025-03-21 19:36:10 +0300

    может конец

--- snake.js
+++ snake.js
@@ -8,10 +8,12 @@ let snake = [{ x: 10, y: 10 }];
 let direction = { x: 0, y: 0 };
 let food = { x: 15, y: 15 };
 let bfood = {x:-1,y:-1};
+let bonuc = {x:-2,y:-2};
 let score = 0;
 let hscore=0;
 let gameInterval;
-
+let a=5;
+let b=0;
 document.addEventListener('keydown', changeDirection);
 document.getElementById('restartButton').addEventListener('click', resetGame);
 
@@ -51,6 +53,7 @@ function updateGame() {
     }
     // Проверка столкновения со стенами
     if (head.x < 0 || head.x >= tileCount || head.y < 0 || head.y >= tileCount) {
+        
         resetGame();
         return;
     }
@@ -58,6 +61,7 @@ function updateGame() {
     // Проверка столкновения с самой собой
     for (let i = 1; i < snake.length; i++) {
         if (snake[i].x === head.x && snake[i].y === head.y) {
+            
             resetGame();
             return;
         }
@@ -68,43 +72,79 @@ function updateGame() {
     // Проверка наличия еды
     if (head.x === food.x && head.y === food.y) {
         score++;
-        document.getElementById('score').textContent = score;
-        placeFood();
+        b++;
         if(score%5===0 && score!= 0)
-    {
-        placeBonucFood();
+        {
+            placeBonucFood();
+            
+        }
+        document.getElementById('score').textContent = score;
         
-    }
+        placeFood();
+        
     } else {
         snake.pop();
     }
+
+    if (head.x === bonuc.x && head.y === bonuc.y) {
+        
+        ctx.clearRect(bonuc.x, bonuc.y, 1, 1);
+        bonuc.x=-2;
+        bonuc.y=-2;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, 130);
+        setTimeout(bon,5000);
+        if((Math.floor(Math.random() * 3))===1){
+            clearInterval(gameInterval);
+            gameInterval = setInterval(updateGame, 130);
+            setTimeout(bon,5000);
+        }
+    }
     if (head.x === bfood.x && head.y === bfood.y) {
+        
         score=score+2;
+        bfood.x=-1;
+        bfood.y=-1;
+
+    
         ctx.clearRect(bfood.x, bfood.y, 1, 1);
+        if((Math.floor(Math.random() * 3))===1){
+            placeBonuc();
+        }
+        
+            
+    
+        
         document.getElementById('score').textContent = score;
-        placeBonucFood();
+        
         
     }
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Отрисовка змейки
-    snake.forEach((segment, index) =>  {
-        if (index === 0) {
-            drawRect(segment.x, segment.y, 'darkgreen'); // Голова змейки
+    for (let i = 0; i < snake.length; i++) {
+        const segment = snake[i];
+        if (i === 0) {
+            drawRect(segment.x, segment.y, 'darkgreen'); // Голова
         } else {
-            drawRect(segment.x, segment.y, 'green'); // Тело змейки
+            drawRect(segment.x, segment.y, 'green'); // Тело
         }
-    });
+    }
 
     // Отрисовка еды
-    if(score%5===0 && score!= 0)
-    {
-        drawRect(bfood.x, bfood.y, 'gold');
-        
-    }
+   
     drawRect(food.x, food.y, 'red');
     
+    if(score%5===0 && score!= 0&&a===b)
+    {
+        placeBonucFood();
+        a=a+5;
+    }
+    drawRect(bfood.x, bfood.y, 'gold');
+        
+    
+    drawRect(bonuc.x, bonuc.y, 'blue');
     
 }
 
@@ -122,11 +162,7 @@ function placeFood() {
         }
     }
 }
-function nul(x,y) {
-    n = {x,y};
 
-    
-}
 
 function placeBonucFood() {
     bfood = {
@@ -142,14 +178,40 @@ function placeBonucFood() {
         }
     }
 }
+function bon(){
+    
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, 100);
+        
+        
+}
+function placeBonuc() {
+    bonuc = {
+        x: Math.floor(Math.random() * tileCount),
+        y: Math.floor(Math.random() * tileCount)
+    };
+
+    // Убедимся, что еда не появляется внутри змейки
+    for (let segment of snake) {
+        if (segment.x === bonuc.x && segment.y === bonuc.y) {
+            placeBonuc();
+            return;
+        }
+    }
+    
+}
 
 
 function resetGame() {
     clearInterval(gameInterval);
     snake = [{ x: 10, y: 10 }];
     direction = { x: 0, y: 0 };
+    bfood.x=-1;
+    bfood.y=-1;
+    bonuc.x=-2;
+    bonuc.y=-2;
     score = 0;
-    
+    a=5;
     document.getElementById('score').textContent = score;
     
     gameInterval = setInterval(updateGame, 100);
